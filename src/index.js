1a01d2af0faa6e7063c69bfb6a1644b63952f1ce
Vimmi-fox <67700467+Vimmi-fox@users.noreply.github.com>
2020-10-01 22:45:55 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    let minNumber = array && array.length > 0 ? array[0] : 0; 
+    if (array) {
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < minNumber) {
+                minNumber = array[i];
+            }
+        }
+    }
+    return minNumber;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let maxNumber = array && array.length > 0 ? array[0] : 0;
+    if (array) {
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > maxNumber) {
+                maxNumber = array[i];
+            }
+        }
+    }
+    return maxNumber;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let avgNumber = 0;
+    if (array && array.length) {
+        for (let i = 0; i < array.length; i++) {
+            avgNumber += array[i];
+        }
+        avgNumber /= array.length;
+    }
+    return avgNumber;
+};
